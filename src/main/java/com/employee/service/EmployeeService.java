4bb30d4d26a98473c95970ed3330e8e46340ab45
Chamodh Jayasumana <chamodhinduwarajayasumana@gmail.com>
2024-08-21 22:40:23 +0530

    define funjions of the each methods in EmplyeeService

--- src/main/java/com/employee/service/EmployeeService.java
+++ src/main/java/com/employee/service/EmployeeService.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public interface EmployeeService {
 
-    List<Employee> getAllEmployee();
+    List<Employee> getAllEmployee(); //use to get all employee
 
-    void save(Employee employee);
+    void save(Employee employee); //use to save new employee
 
-    Employee getById(Long id);
+    Employee getById(Long id); //use to get employee data based on employee id
 
-    Void deleteById(Long id);
+    Void deleteById(Long id); //this is used to delete employee by using employee id
 
 }
